2ce5ba68db6cdebb206a9d9e954eb2ced7b37be1
Russell Hunter <rih053@yahoo.co.uk>
2021-02-09 02:37:45 +0000

    Added the Chatroom component.

--- chatroom/src/App.js
+++ chatroom/src/App.js
@@ -1,9 +1,11 @@
 import Login from './components/Login'
+import Chatroom from './components/Chatroom'
 
 function App() {
   return (
     <div>
       <Login />
+      <Chatroom />
     </div>
   );
 }
---
+++ chatroom/src/components/Chatroom.js
@@ -0,0 +1,10 @@
+
+function Chatroom() {
+    return (
+      <div>
+        <h1>Chatroom</h1>
+      </div>
+    );
+  }
+  
+  export default Chatroom;
