cd628e975d6eda80fd3b7d90b4359dc5c4ecab06
Russell Hunter <rih053@yahoo.co.uk>
2021-02-14 22:05:15 +0000

    APP-00: Added login screen.

--- chatroom/src/components/Login.js
+++ chatroom/src/components/Login.js
@@ -1,8 +1,26 @@
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div style={{ display: 'flex', flex: 1, height: '100vh' }}>
+      <div style={{ 
+          display: 'flex', 
+          flex: 1, 
+          justifyContent: 'center', 
+          alignItems: 'center', 
+          minWidth: 300 
+        }}>
+            <button style={{ 
+                backgroundColor: 'red', 
+                borderWidth: 0, 
+                paddingTop: 12, 
+                paddingBottom: 12, 
+                paddingLeft: 24, 
+                paddingRight: 24, 
+                color: 'white', 
+                fontSize: 18, 
+                fontWeight: 'bold' 
+                }}>Login with Google</button>
+      </div>
     </div>
   );
 }
