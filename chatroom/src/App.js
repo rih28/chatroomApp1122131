55278e92b9506f898ad602e97cf74b1b65e1483c
Russell Hunter <rih053@yahoo.co.uk>
2021-02-14 22:25:23 +0000

    Added initial routing.

--- chatroom/src/App.js
+++ chatroom/src/App.js
@@ -1,11 +1,20 @@
 import Login from './components/Login'
 import Chatroom from './components/Chatroom'
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Login />
-      <Chatroom />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/chatroom" component={Chatroom} />
+        </Switch>
+      </Router>
     </div>
   );
 }
